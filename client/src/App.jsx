fecd52e6db9734e4d36efd0c1a390810a543e589
tunkrawaysoe <57566289+tunkrawaysoe@users.noreply.github.com>
2025-04-26 13:07:15 +0630

    fix: import createTheme correctly and optimize performance with useMemo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,32 @@
-import {BrowserRouter as Router , Route, Routes,Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './scenes/HomePage';
 import LoginPage from './scenes/Login';
 import ProfilePage from './scenes/ProfilePage';
 import RegisterPage from './scenes/RegisterPage';
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material'; 
+import { useSelector } from 'react-redux';
+import { themeSettings } from './theme';
+import { useMemo } from 'react';
+
 function App() {
-  
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
-    <>
-      <div className='app'>
-        <Router>
+    <div className="app">
+      <Router>
+        <ThemeProvider theme={theme}>
+          <CssBaseline /> {/* Ensuring CssBaseline is applied correctly */}
           <Routes>
-            <Route path='/' element={<LoginPage/>}/>
-            <Route path='/home' element={<HomePage/>}/>
-            <Route path='/profile/:userId' element={<ProfilePage/>}/>
-            <Route path='/register' element={<RegisterPage/>}/>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+            <Route path="/register" element={<RegisterPage />} />
           </Routes>
-        </Router>
-        
-      </div>
-     
-    
-    </>
-  )
-  
+        </ThemeProvider>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
