4ff6841f967d68db4b6514e492231d51ac01f3ed
tunkrawaysoe <57566289+tunkrawaysoe@users.noreply.github.com>
2025-05-02 20:14:12 +0630

    Fix and put all the things properly in user session

--- client/src/scenes/HomePage.jsx
+++ client/src/scenes/HomePage.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import NavBar from '../navbar/index';
-import { Box } from '@mui/material';
+import { Box, useMediaQuery } from '@mui/material';
 import UserWidget from './Widgets/UserWidget';
+import { Block } from '@mui/icons-material';
+import { useSelector } from 'react-redux';
+import PostWidget from './Widgets/PostWidget';
 
 const HomePage = () => {
-
+  const isNonMobileScreens = useMediaQuery('(min-width : 1000px)')
   return (    
     <Box>
       <NavBar />
-      <UserWidget/>
-    
-      
+      <Box
+        width='100%' 
+        padding='2rem 6%' 
+        display={isNonMobileScreens ? 'flex' : 'block'}
+        justifyContent='space-between'
+        gap='2rem'
+
+      >
+        <Box flexBasis={isNonMobileScreens ? '26%' :  undefined}>
+          <UserWidget/> 
+        </Box>
+        <Box flexBasis={isNonMobileScreens ? '42%' :  undefined}>
+          <PostWidget/> 
+        </Box>
+        
+        
+      </Box>
     </Box>
   );
 }
--- client/src/scenes/Widgets/UserWidget.jsx
+++ client/src/scenes/Widgets/UserWidget.jsx
@@ -57,18 +57,90 @@ const UserWidget = ({userId,picturePath}) => {
        gap='1rem'>
         <FlexBetween
         gap='1rem'>
-          <UserImage image={picturePath}/>
+          <UserImage imgsrc='../../src/post8.jpeg'/>
           <Box>
             <Typography>
               {firstName} {lastName}
             </Typography>
-            <Typography>
+            <Typography color={medium}>
              {0} {friends.length < 1 ? "friend" : 'friends'}
             </Typography>
           </Box>
         </FlexBetween> 
         <ManageAccountsOutlined/>
       </FlexBetween>
+      <Divider/>
+      {/* Second Row*/}
+      <Box padding='1rem 0'>
+        <Box display='flex'  gap='1rem' alignItems='center'mb='0.5rem'>
+          <LocationOnOutlined/>
+          <Typography color={medium} >
+            {location}
+          </Typography>
+        </Box>
+        <Box display='flex' gap='1rem' alignItems='center'>
+          <WorkOutlineOutlined/>
+          <Typography color={medium}>
+            {occupation}
+          </Typography>
+        </Box>
+       </Box>
+       <Divider/>
+       {/* Third Row*/}
+       <Box padding='1rem 0'>
+        <FlexBetween mb='0.5rem'>
+          <Typography color={medium}>
+            Who's view your profile
+          </Typography>
+          <Typography>
+            {viewedProfile}
+          </Typography>
+        </FlexBetween>
+        <FlexBetween>
+        <Typography color={medium}>
+            Impressions of your post
+          </Typography>
+          <Typography>
+            {impressions}
+          </Typography>
+        </FlexBetween>
+       </Box>
+       <Divider/>
+       {/* Fourth Row */}
+       <Box padding='1rem 0'>
+        <Typography mb='0.5rem'>
+          Social Profiles
+        </Typography>
+        <FlexBetween>
+          <FlexBetween gap='1rem' mb='0.5rem' >
+            <img src='../../src/assets/twitter.png' alt='twitter'/>
+            <Box>
+              <Typography>
+                Twitter
+              </Typography>
+              <Typography color = {medium}>
+                Social Network
+              </Typography>
+            </Box>
+          </FlexBetween>
+          <EditOutlined/>
+        </FlexBetween>  
+        <FlexBetween>
+          <FlexBetween gap='1rem'>
+            <img src='../../src/assets/twitter.png' alt='twitter'/>
+            <Box>
+              <Typography>
+                Twitter
+              </Typography>
+              <Typography color = {medium}>
+                Social Network
+              </Typography>
+            </Box>
+          </FlexBetween>
+          <EditOutlined/>
+        </FlexBetween>   
+       </Box>
+       
     </WidgetWrapper>
   )
 }
